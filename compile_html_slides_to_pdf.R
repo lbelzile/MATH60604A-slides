8e9c904831684a5f19d4a15758cbe02cb8b7b7c9
Léo Belzile <belzilel@gmail.com>
2021-02-01 10:13:02 -0500

    Xaringan slides with panelset correctly printed

--- compile_html_slides_to_pdf.R
+++ compile_html_slides_to_pdf.R
@@ -1,5 +1,21 @@
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# files <- list.files(pattern = "^MATH60604A.*\\.Rmd$", full.names = TRUE)
+# for(file in files){
+#   pagedown::chrome_print(file)
+# }
+
+## NEW solution that prints panelsets properly
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-files <- list.files(pattern = "^MATH60604A.*\\.Rmd$", full.names = TRUE)
+# source("https://git.io/xaringan2pdf")
+library(xaringanBuilder)
+# library(chromote)
+files <- paste0("https://lbelzile.github.io/MATH60604A-slides/",
+                list.files(include.dirs = TRUE,
+                            pattern = "^MATH60604A.*\\.html$",
+                            full.names = FALSE))
 for(file in files){
-  pagedown::chrome_print(file)
+  xaringanBuilder::xaringan_to_pdf(input = file,
+                  output_file = paste0(tools::file_path_sans_ext(basename(file)), ".pdf"),
+                  delay = 0.15,
+                  include_partial_slides = FALSE)
 }
